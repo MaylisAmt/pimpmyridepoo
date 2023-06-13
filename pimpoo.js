249c3eacdfcb739af4ebe299fa38206a477fa800
Coquelicot49 <coraline.jadeau@gmail.com>
2023-06-13 16:09:25 +0200

    Etape 2 : Fonction qui retourne plusieurs instances de l'objet Trip

--- pimpoo.js
+++ pimpoo.js
@@ -1,5 +1,6 @@
-// Étape 1 - Parsing
+////// Étape 1 - Parsing
 
+// CRÉATION DE LA CLASS
 class Trip {
 
     constructor(name, start, duration, price) {
@@ -8,15 +9,45 @@ class Trip {
         this.duration = duration
         this.price = price
     }
-
 }
 
+// FONCTION QUI TRANSFORME LES DONNÉES D'UN CLIENT EN 1 INSTANCE DE "Trip"
 function parseTrip (trip) {
     let tripToParse = trip.split(" ")
     const newObj = new Trip (tripToParse[0],tripToParse[1],tripToParse[2],tripToParse[3])
     return newObj
 }
-console.log(parseTrip("Roger 0 5 10"))
+// console.log("etape 1", parseTrip("Roger 0 5 10"))
+// > Résultat console : Trip { name: 'Roger', start: '0', duration: '5', price: '10' }
 
 
+////// Etape 2 - Loop Parsing
 
+// Liste des premiers clients
+let tripsToParse = [
+	"Roger 0 5 10",
+	"Pongo 3 7 14",
+	"Perdita 8 10 8",
+	"Anita 16 3 7"
+]
+
+
+// FONCTION QUI TRANSFORME LES DONNÉES DE PLUSIEURS CLIENTS EN PLUSIEURS INSTANCES DE "Trip"
+function parseTrips (trips) {
+    let tableau = []
+    for (let i=0; i<trips.length; i++) {
+        tableau.push((parseTrip(trips[i])))
+    }
+    return tableau
+}
+
+// console.log("etape 2", parseTrips(tripsToParse))
+// > Résultat console : [
+//   Trip { name: 'Roger', start: '0', duration: '5', price: '10' },
+//   Trip { name: 'Pongo', start: '3', duration: '7', price: '14' },
+//   Trip { name: 'Perdita', start: '8', duration: '10', price: '8' },
+//   Trip { name: 'Anita', start: '16', duration: '3', price: '7' }
+// ]
+
+
+////// Étape 3 - End trip
